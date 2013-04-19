7985cae597bbba44f5abb86e3e4b8db0be390767
nick <eiu165@gmail.com>
2013-04-18 22:53:51 -0500

    added tailspin toys for a sample

---
+++ src/Tailspin.Business/IToyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tailspin.Data;
+using Tailspin.Entity;
+
+namespace Tailspin.Business
+{
+
+    public interface IToyService
+    {
+        IEnumerable<Toy> Get();
+    }
+    public class ToyService : IToyService
+    {
+        public IToyRepository _toyRepository;
+        public ToyService(IToyRepository toyRepository)
+        {
+            this._toyRepository = toyRepository;
+        }
+        public IEnumerable<Toy> Get()
+        {
+            return _toyRepository.Get();
+        }
+    }
+}
---
+++ src/Tailspin.Data/IToyRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tailspin.Entity;
+
+namespace Tailspin.Data
+{
+    public interface IToyRepository
+    {
+        IEnumerable<Toy> Get();
+    }
+
+
+    public class ToyRepository : IToyRepository
+    {
+        private string _connectionString;
+        public ToyRepository(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+        public IEnumerable<Toy> Get()
+        {
+            var list = new List<Toy>();
+            list.Add(new Toy { Name = "Airplane", Description = "wind up", Price = 94.3 });
+            return (list);
+        }
+    }
+
+}
